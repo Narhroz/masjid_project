2a86fa1cdebe640795ee26aedcbfee8286c9d040
TokaTaka <110289565+Narhroz@users.noreply.github.com>
2022-12-05 09:31:27 +0700

    Add files via upload

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,32 @@
+<!-- footer -->
+<div class="footer">
+    <div class="container">
+        <div class="navbar-brand footer-navbar">Al-Furqon</div>
+        <div class="footer-content">
+            <div class="social-about">
+                <i class="fa-solid fa-location-dot"></i>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et assumenda libero, vero dicta a eos architecto voluptatum sequi ut enim.</p>
+                <i class="fa-solid fa-phone"></i>
+                <p>+1-543-123-4567</p>
+                <i class="fa-solid fa-envelope"></i>
+                <p>example@huddle.com</p>
+            </div>
+            <div class="social-method">
+                <p>About Us</p>
+                <p>What We Do</p>
+                <p>FAQ</p>
+            </div>
+            <div class="social-method">
+                <p>Carrer</p>
+                <p>Blog</p>
+                <p>Met us</p>
+            </div>
+            <div class="social-media">
+                <i class="fa-brands fa-facebook-f"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- akhir footer -->
---
+++ resources/views/partials/nav_index.blade.php
@@ -0,0 +1,21 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <a class="navbar-brand" href=".">Al-Furqon</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto">
+                <a class="nav-link" href=".">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="/tentang">Tentang Kami</a>
+                <a class="nav-link" href="#">News</a>
+                <a class="nav-link" href="#">Educational</a>
+                <a class="btn btn-success tombol tombol-atas" href="#">Donate</a>
+            </div>
+        </div>
+    </div>
+</nav>
+<!-- akhir navbar -->
+
+
